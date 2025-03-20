682084167c32f856fff430072b97328c526ac627
alteredtech <michael@einreinhof.com>
2025-03-20 10:42:09 -0600

    Checking docker is running and ensuring sam logs arent added

--- test_locally.sh
+++ test_locally.sh
@@ -5,6 +5,12 @@ set -o pipefail  # Exit if any part of a pipeline fails
 
 trap 'echo "Stopping local API Gateway..."; pkill -f "sam local start-api"' EXIT
 
+echo "Checking if Docker is running..."
+if ! docker info > /dev/null 2>&1; then
+    echo "Docker is not running. Please start Docker and try again."
+    exit 1
+fi
+
 echo "Building Swift package archive..."
 swift package archive --verbose --allow-network-connections docker
 
@@ -15,7 +21,7 @@ echo "Starting local API Gateway..."
 sam local start-api --template template.yml --warm-containers EAGER --disable-authorizer 2>&1 | tee sam_logs.txt &
 
 # Give some time for the API Gateway to start
-sleep 15
+sleep 120
 
 echo "Running Schemathesis Explicit Example test..."
 schemathesis run ./Sources/openapi.yaml --base-url 'http://127.0.0.1:3000' \
@@ -27,7 +33,7 @@ echo "Running Schemathesis Set Seed test..."
 schemathesis run ./Sources/openapi.yaml --base-url 'http://127.0.0.1:3000' \
 --hypothesis-seed=47789167578022855610497879068613925682 \
 --generation-allow-x00=false \
---data-generation-method=all \
+--data-generation-method=positive \
 --checks=all
 
 echo "Running Schemathesis Random test..."
